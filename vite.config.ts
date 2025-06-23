3d93f4fcce176503c12c9f4e5abf9bafc8fdaec6
Amr Elmawardy <amr.elmawardy94@gmail.com>
2025-06-24 02:39:59 +0300

    fix: oidc module path

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path'; // Import Node.js path module
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -33,7 +34,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue-oidc-client/vue3': '/home/elmawardy/projects/nutrix/posui/node_modules/vue-oidc-client/vue3/src/vue-oidc-client.ts', // Non-minified path
+      'vue-oidc-client/vue3': path.resolve(__dirname, './node_modules/vue-oidc-client/vue3/src/vue-oidc-client.ts'), // Non-minified path
     },
   },
 })
